fe04d9eb7c205bf870bc21ddebaef68c5a8b55ca
Excse <timo@DESKTOP-RLBI982>
2024-01-22 19:30:04 +0100

    Added custom zsh configs

---
+++ .oh-my-zsh/custom/z_aliases.zsh
@@ -0,0 +1,3 @@
+alias cd='z'
+alias vim='lvim'
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
---
+++ .oh-my-zsh/custom/z_p10k.zsh
@@ -0,0 +1,5 @@
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+source ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
